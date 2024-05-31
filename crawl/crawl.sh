b58747acbf8b995f2ebb6f2bffedfc2701d6b38a
hooNpk <hoonpk96@gmail.com>
2024-06-01 04:23:42 +0900

    feat(Docker-crawl.sh-main.py): 도커파일을 사용. 컨테이너 내에서 크롤링하여 csv 파일을 만들고 이를 host 디렉토리에 저장

---
+++ crawl/crawl.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+CUR_DIR=$(pwd)
+
+mkdir /sharespace
+
+IMAGE_NAME="hoonpk/tn-crawl:0.1"
+# 이미지가 존재하는지 확인
+if docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
+    echo "이미지 $IMAGE_NAME가 이미 존재합니다."
+else
+    echo "이미지 $IMAGE_NAME가 존재하지 않습니다. 이미지를 pull합니다."
+    docker pull "$IMAGE_NAME"
+fi
+
+#백그라운드에서 docker container tn-crawl 실행
+docker run -d --rm --name tn-crawl -v $CUR_DIR/sharespace:/sharespace hoonpk/tn-crawl:0.1
--- crawl/main.py
+++ crawl/main.py
@@ -1,27 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup, NavigableString
-#import pandas as pd
-from datetime import datetime
+import pandas as pd
+import traceback
 
-def get_notices(base_url, start_date, end_date):
+def get_skku_notices(base_url, start_date, end_date):
     notices = []
     page = 1
-    while True:
-        response = requests.get(f"{base_url}{(page-1)*10}")
+    date_text = start_date
+    while start_date <= date_text: # 날짜가 시작 날짜보다 이전이면 크롤링 종료
+        try:
+            response = requests.get(f"{base_url}{(page-1)*10}")
+        except Exception as e:
+            print(traceback.format_exc())
+            print(e)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # 공지사항 리스트 추출
         notice_elements = soup.select_one(
             '#jwxe_main_content > div > div > div.container > div.board-name-list.board-wrap > ul'
         ).children
-        
+
+
         if not notice_elements:
             break
 
         for notice in notice_elements:
             if isinstance(notice, NavigableString):
                 continue
-
             ctgry = notice.select_one('dl > dt > span.c-board-list-category')
             ctgry_text = ctgry.text.strip() if ctgry else None
             title = notice.select_one('dl > dt > a')
@@ -39,25 +46,32 @@ def get_notices(base_url, start_date, end_date):
                     "date": date_text,
                     "url": full_link,
                     "writer" : writer_text,
-                    "category" : ctgry_text
+                    "category" : ctgry_text,
+                    "source" : "skku"
                 })
             else:
-                return notices  # 날짜가 시작 날짜보다 이전이면 크롤링 종료
+                continue  
         page += 1
     return notices
 
 # 크롤링할 URL 및 날짜 범위 설정
-url = "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&&articleLimit=10&article.offset="
+skku_url = "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&&articleLimit=10&article.offset="
 start_date = "2024-05-30"  # 시작 날짜
 end_date = "2024-05-31"  # 끝 날짜
 
 # 공지사항 크롤링
-notices = get_notices(url, start_date, end_date)
-print(notices)
+notices = get_skku_notices(skku_url, start_date, end_date)
 
+# TODO 소프트웨어대학, 소프트웨어학과 공지사항 크롤링
+# 소프트웨어대학 공지사항 : https://sw.skku.edu/sw/notice.do?mode=list&&articleLimit=10&article.offset=20
+# 소프트웨어학과 공지사항 : https://cse.skku.edu/cse/notice.do
+
+# 이걸 데이터프레임 형태로 만들어서 csv 저장. 
+# 현재 디렉토리 기준으로 해서 volume을 설정해야 할 듯
+# mysql 관련 컨테이너에서 집어넣자
 # # 데이터프레임으로 변환 후 출력
-# df = pd.DataFrame(notices)
-# print(df)
+df = pd.DataFrame(notices)
 
-# # 결과를 csv 파일로 저장 (선택 사항)
-# df.to_csv('skku_notices.csv', index=False)
+#결과를 csv 파일로 저장
+#csv를 읽어와서 mysql database에 넣어주는 건 다른 container에서 실행
+df.to_csv('/sharespace/skku_notices_240531-240531.csv', index=False, encoding='utf-8')
