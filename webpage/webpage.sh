a066f0bd355c7ad8faaa4db2c947bdb4febab5ec
hooNpk <hoonpk96@gmail.com>
2024-06-03 13:30:39 +0900

    fix(webpage.sh): $IMAGE_NAME 오류 해결

--- webpage/webpage.sh
+++ webpage/webpage.sh
@@ -1,11 +1,11 @@
 # Run backend
 BACK_IMAGE_NAME="hoonpk/tn-back:0.2"
 # 이미지가 존재하는지 확인
-if docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
-    echo "이미지 $IMAGE_NAME가 이미 존재합니다."
+if docker image inspect "$BACK_IMAGE_NAME" > /dev/null 2>&1; then
+    echo "이미지 $BACK_IMAGE_NAME 이미 존재합니다."
 else
-    echo "이미지 $IMAGE_NAME가 존재하지 않습니다. 이미지를 pull합니다."
-    docker pull "$IMAGE_NAME"
+    echo "이미지 $BACK_IMAGE_NAME 존재하지 않습니다. 이미지를 pull합니다."
+    docker pull "$BACK_IMAGE_NAME"
 fi
 
 docker run -d --rm --name tn-back \
@@ -17,11 +17,11 @@ docker run -d --rm --name tn-back \
 # Run frontend
 FRONT_IMAGE_NAME="hoonpk/tn-front:0.3"
 # 이미지가 존재하는지 확인
-if docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
-    echo "이미지 $IMAGE_NAME가 이미 존재합니다."
+if docker image inspect "$FRONT_IMAGE_NAME" > /dev/null 2>&1; then
+    echo "이미지 $FRONT_IMAGE_NAME 이미 존재합니다."
 else
-    echo "이미지 $IMAGE_NAME가 존재하지 않습니다. 이미지를 pull합니다."
-    docker pull "$IMAGE_NAME"
+    echo "이미지 $FRONT_IMAGE_NAME 존재하지 않습니다. 이미지를 pull합니다."
+    docker pull "$FRONT_IMAGE_NAME"
 fi
 
 docker run -d --rm --name tn-front -p 80:80 "$FRONT_IMAGE_NAME"
